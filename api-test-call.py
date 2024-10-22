4991d07762c9bcc3e20b712de8a4352dd042e130
Pierre Bourreau <pbourreau@nobatek.inef4.com>
2024-10-22 17:13:26 +0200

    Change Gaz for Gas

--- api-test-call.py
+++ api-test-call.py
@@ -20,22 +20,33 @@ def _test_users():
 
 def _test_projects():
     try:
-        print("TEST 1: create a new project")
-        response = requests.post('http://127.0.0.1:5000/project/1006', auth=('john', 'hello'))
-        print(response.json())
-        response = requests.get('http://127.0.0.1:5000/scenarios/1006', auth=('john', 'hello'))
+        #print('Test from Tecnalia')
+        response = requests.post('https://genscen.ensnare.nobatek.com/project/1047', auth=('pierre', 'ensn@re'))
         print(response.json())
 
-        resp = requests.get('http://127.0.0.1:5000/scenarios/1234', auth=('john', 'hello'))
-        print(resp.json())
-        print('---------------------')
-        resp = requests.get('http://127.0.0.1:5000/scenarios/1234', auth=('susan', 'beauty'))
-        print(resp.json())
+        response = requests.get('https://genscen.ensnare.nobatek.com/scenarios/1047', auth=('pierre', 'ensn@re'))
+        #response = requests.get('http://127.0.0.1:5000/scenarios/1047', auth=('john', 'hello'))
+        print(response.json())
 
-        resp = requests.delete('http://127.0.0.1:5000/project/1234', auth=('susan', 'beauty'))
+        response = requests.delete('https://genscen.ensnare.nobatek.com/project/1047', auth=('pierre', 'ensn@re'))
+        print(response.json())
+
+        # print("TEST 1: create a new project")
+        #response = requests.post('http://127.0.0.1:5000/project/1006', auth=('john', 'hello'))
+        #print(response.json())
+        #response = requests.get('http://127.0.0.1:5000/scenarios/1006', auth=('john', 'hello'))
+        #print(response.json())
+
+        #resp = requests.get('http://127.0.0.1:5000/scenarios/1234', auth=('john', 'hello'))
+        #print(resp.json())
+        #print('---------------------')
+        #resp = requests.get('http://127.0.0.1:5000/scenarios/1234', auth=('susan', 'beauty'))
+        #print(resp.json())
+
+        #resp = requests.delete('http://127.0.0.1:5000/project/1234', auth=('susan', 'beauty'))
     except Exception as e:
         print(str(e))
 
 if __name__ == "__main__":
-    _test_users()
-    # _test_projects()
+    # _test_users()
+    _test_projects()
